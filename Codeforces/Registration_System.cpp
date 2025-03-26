800e14cdc133c65ead1e7df8d9a1f980b71e2197
sa-masum <sa.masum125@gmail.com>
2025-03-26 07:14:03 +0600

    Registration_System

---
+++ Codeforces/Registration_System.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fasty ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define endl '\n'
+#define pb push_back
+#define ll long long
+#define ld long double
+
+template<typename T>
+using vec = vector<T>;
+
+template<typename T1, typename T2>
+using pv = pair<vector<T1>, vector<T2>>;
+
+template<typename T1, typename T2>
+using pr = pair<T1, T2>;
+
+template<typename T1, typename T2>
+using vecp = vector<pr<T1, T2>>;
+
+int main(){
+    fasty;
+    int n;
+    cin >> n;
+    string str[n];
+    map<string, int> count;
+    for(int i=0; i<n; i++){
+        cin >> str[i];
+        count[str[i]]++;
+        if(count[str[i]] == 1){
+            cout << "OK" << endl;
+        }else{
+            cout << str[i] << (count[str[i]] - 1) << endl;
+        }
+    }
+    return 0;
+}
